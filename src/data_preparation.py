7277ee085b0232f1827b9b354ac47f9a87ff3dc3
ISBachvarov21 <ISBachvarov21@codingburgas.bg>
2024-02-21 18:09:12 +0200

    Add scraped data storage

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -12,12 +12,24 @@ response: requests.Response = requests.get(url)
  
 soup: bs4.BeautifulSoup = bs4.BeautifulSoup(response.content, 'html.parser')
 
-text: bs4.element.ResultSet[any] = soup.find_all('p')
+text_elements: bs4.element.ResultSet[any] = soup.find_all('p')
+
+text: str = ''
  
-for p in text:
-    p_str: str = p.get_text()
+for paragraph in text_elements:
+    paragraph_str: str = paragraph.get_text()
  
-    if p_str.isspace() or not p_str.strip():
+    if paragraph_str.isspace() or not paragraph_str.strip():
         continue
  
-    print(f'\033[1;34m[*] Paragraph found: {p_str}\033[0m\n')
+    text += paragraph_str + '\n'
+
+print(f'\033[1;32m[+] Text found: {text}\033[0m\n')
+print(f'\033[1;34m[*] Trying to write the text to data/scraped/data.txt...\033[0m')
+
+try:
+    with open('data/scraped/data.txt', 'w', encoding='utf-8') as file:
+        file.write(text)
+    print(f'\033[1;32m[+] Text written to file successfully!\033[0m')
+except Exception as e:
+    print(f'\033[1;31m[-] An error occurred while writing to data/scraped/data.txt: {e}\033[0m')
