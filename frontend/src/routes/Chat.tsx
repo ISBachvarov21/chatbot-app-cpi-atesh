2400e4047cc0c49dcace2e005acab796c403836e
IDDochev21 <IDDochev21@codingburgas.bg>
2024-06-25 23:21:31 +0300

    Add response message

--- frontend/src/routes/Chat.tsx
+++ frontend/src/routes/Chat.tsx
@@ -14,14 +14,31 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { MessageType, ChatType } from "@/types/chat.type";
+import ResponseMessage from "@/components/ResponseMessage";
 
 export default function Chat() {
     const [user, setUser] = useState<string>("User");
     const [chatHistory, setChatHistory] = useState<any>([]);
-    const [chatMessages, setChatMessages] = useState<any>([]);
     const [newChatName, setNewChatName] = useState<string>("");
 
     let [message, setMessage] = useState<string>("");
+
+    const [selectedChat, setSelectedChat] = useState<ChatType | undefined>(
+        {
+        "name": "Atesh ma maiko", 
+        "messages": [
+                {
+                    "author": "USER",
+                    "content": "Skibidi, skibidi on the wall, who's the gyattest of them all"
+                },
+                {
+                    "author": "AI",
+                    "content": "Ivaylo"
+                },
+            ]
+     
+        }
+    )
 
     const handelNewMessage = (e: any) => {
         e.preventDefault();
@@ -30,10 +47,10 @@ export default function Chat() {
     return (
         <>
             <div
-                className={`min-w-screen overflow-hidden h-screen bg-gradient-to-b from-[#1E1E1E] to-[#6618E7] flex`}
+                className={`min-w-screen overflow-hidden h-screen bg-gradient-to-b bg-gray-300 flex`}
             >
                 <div
-                    className={`w-1/5 bg-black text-white min-h-[80%] items-center flex flex-col my-5 ml-5 rounded-2xl`}
+                    className={`w-1/5  bg-gray-900 text-white min-h-[80%] items-center flex flex-col my-5 ml-5 rounded-2xl`}
                 >
                     <div className="w-[90%] h-full mt-12 flex flex-col items-center">
                         <div className="flex gap-4 items-center border border-pink-50 w-full p-2 px- 4 rounded-[36px]">
@@ -94,40 +111,23 @@ export default function Chat() {
                     </div>
                 </div>
                 <div className="w-4/5 min-h-[80%] items-center flex flex-col m-5 relative rounded-2xl text-white">
-                    <div
-                        className={`text-white w-full h-5/6 flex flex-col p-4 ml-[300px] gap-2`}
-                    >
-                        <ScrollArea
-                            className={`gap-2 flex flex-col font-bold ${chatMessages.length == 0 ? "" : "bg-black"} p-8 rounded-3xl w-[70%]`}
-                        >
-                            {chatMessages.map((message: any, index: any) => {
-                                if (index % 2 == 0)
-                                    return (
-                                        <div className="w-[70%] h-[12%] rounded-xl flex gap-5 p-3 items-center">
-                                            <p className="text-white break-words">
-                                                {message[2]}
-                                            </p>
-                                        </div>
-                                    );
-                                else
-                                    return (
-                                        <div className="w-[70%] h-[12%] rounded-xl flex gap-5 p-3 items-center">
-                                            <p className="text-white ml-10 mt-[-10px] break-words">
-                                                {message[2]}
-                                            </p>
-                                        </div>
-                                    );
-                            })}
-                        </ScrollArea>
+
+                    <div className={`text-white w-full h-5/6 flex flex-col p-16 gap-6`}>
+                        {
+                            selectedChat?.messages.map((item: MessageType) => {
+                                return <ResponseMessage author={item.author} content={item.content} />
+                            })
+                        }
+
                     </div>
 
                     <form
                         onSubmit={handelNewMessage}
-                        className={`bg-white text-black w-[70%] h-[12%] rounded-[36px] flex gap-5 p-6 items-center`}
+                        className={`bg-gray-900 text-black w-[70%] h-[12%] rounded-xl flex gap-5 p-6 items-center`}
                     >
                         <Textarea
                             onChange={(e) => setMessage(e.target.value)}
-                            className="resize-none border-none"
+                            className="resize-none border-none outline-none focus:outline-none"
                         />
                         <Button
                             size="icon"
