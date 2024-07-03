64cd1804b2f9a4db106ef42ac9c1c4688e3191ed
Iva Nedkova <INNedkova21@codingburgas.bg>
2024-07-03 06:43:50 +0300

    Add message and chat endpoints

---
+++ backend/src/api/routes/chats.py
@@ -0,0 +1,65 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+import jwt
+from jwt.exceptions import InvalidTokenError
+import os
+from typing import Annotated
+import db.db_chats
+import db.db_messages
+import models.chat_im
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+router = APIRouter()
+
+@router.get("/get/chats", tags=["chats"])
+async def get_user_chats(token: Annotated[str, Depends(oauth2_scheme)]):
+    try:
+        payload = jwt.decode(token, os.getenv("RSA_PUBLIC_KEY"), algorithms=["RS256"])
+        username: str = payload["iss"]
+
+        chats = db.db_chats.get_chats(username)
+        returnable_chats = []
+
+        for chat in chats:
+            returnable_chats.append({"id": chat["id"], "name": chat["name"]})
+
+        return returnable_chats
+    
+    except InvalidTokenError:
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    
+@router.post("/create/chat", tags=["chats"])
+async def create_user_chat(token: Annotated[str, Depends(oauth2_scheme)], chat: models.chat_im.ChatIM):
+    try:
+        payload = jwt.decode(token, os.getenv("RSA_PUBLIC_KEY"), algorithms=["RS256"])
+        username: str = payload["iss"]
+
+        new_chat = db.db_chats.create_chat(username, chat)
+
+        return new_chat
+    
+    except InvalidTokenError:
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+@router.get("/get/chat/{chat_id}", tags=["chats"])
+async def get_user_chat(token: Annotated[str, Depends(oauth2_scheme)], chat_id: int):
+    try:
+        payload = jwt.decode(token, os.getenv("RSA_PUBLIC_KEY"), algorithms=["RS256"])
+        username: str = payload["iss"]
+
+        chat = db.db_chats.get_chat(username, chat_id)
+        messages = db.db_messages.get_messages(chat_id)
+
+        for i in range(len(messages)):
+            messages[i] = {
+                "content": messages[i]["content"],
+                "author": messages[i]["author"]
+            }
+
+        return {
+            "name": chat["name"],
+            "messages": messages
+        }
+    
+    except InvalidTokenError:
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
