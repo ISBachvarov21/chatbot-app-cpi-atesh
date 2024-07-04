131a8a8fcc6f66e1d88f47b51fb1a0cbe4a6a2bd
IYStoychev21 <IYStoychev21@codingburgas.bg>
2024-07-04 11:42:09 +0300

    Link model to the website

--- frontend/src/components/ResponseMessage.tsx
+++ frontend/src/components/ResponseMessage.tsx
@@ -1,11 +1,11 @@
 
-export default function ResponseMessage({ content, username } : {content: string, username: string | undefined} ) {
+export default function ResponseMessage({ content, author, username } : {content: string, author: string, username: string | undefined} ) {
     
     return (
         <div className="flex gap-8 items-center">
             <div className="h-full">
                 <div className="relative after:content-[''] after:absolute after:right-[-10px] after:bottom-0 after:w-0.5 after:h-full after:z-10 after:bg-gray-900">
-                    <img className="w-12 h-12 rounded-full" src={`https://ui-avatars.com/api/?name=${username}&size=128`} alt="" />
+                    <img className="w-12 h-12 rounded-full" src={`https://ui-avatars.com/api/?name=${author === 'AI'? author: username}&size=128`} alt="" />
                 </div>
             </div>
 
